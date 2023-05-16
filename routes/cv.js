a2ea0a00c9566129c1a81d0e847cfea698442edf
massarrabj <120268474+massarrabj@users.noreply.github.com>
2023-05-16 12:41:01 +0100

    adding changes to cv controller

--- routes/cv.js
+++ routes/cv.js
@@ -1,12 +1,13 @@
 import express from 'express';
 const router = express.Router();
 
-import {createCv, getAllCv, getCv, getCvByIdUser, updateCv} from '../controllers/cv.js';
+import {createCv, getAllCv, getCv, getCvByIdUser, updateCv, getCVByUser} from '../controllers/cv.js';
 
 // all are checked
 router.post('/create', createCv);
 router.get('/getall', getAllCv);
 router.get('/getbyid/:id', getCv);
+router.get('/getbyuser', getCVByUser);
 router.get('/getbyiduser/:iduser', getCvByIdUser);
 router.put('/update/:iduser', updateCv) // ---> params
 
